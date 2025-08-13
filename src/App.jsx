c47f75de299a62859e7948e22c761c7f05283f2b
Gabriel Restrepo <gabo1gara@gmail.com>
2025-08-13 00:15:45 -0700

    Feat: Add completeTodo function to mark tasks as completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,26 @@ function App() {
     const newTodo = {
       title,
       id: Date.now(),
+      isCompleted: false,
     };
     setTodoList([...todoList, newTodo]);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
